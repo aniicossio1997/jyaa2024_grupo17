0812a949c83d5c6b6112ad147bdb810db314511f
Ana Cossio <anacossio7@gmail.com>
2024-07-02 16:50:42 -0300

    added service

--- frontend/src/app/services/materia-prima.service.ts
+++ frontend/src/app/services/materia-prima.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable()
 export class MateriaPrimaService {
   private API_URL ='http://localhost:8080/SalaDeElaboracion/api' ;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  get(){
+    const url = `${this.API_URL}/familiaProductoras`;
+    return this.http.get<any[]>(url)
+      .pipe(
+        map((res: any) => res)
+        );
+  }
 }

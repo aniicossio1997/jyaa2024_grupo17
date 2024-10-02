456abb68f1d7ae3f0e5126fb2306c3df01c2ddf4
Leandro <leandrosvetlich@gmail.com>
2024-10-02 19:36:11 -0300

    fix to price

--- frontend/src/app/shared/pipes/price-transform.pipe.ts
+++ frontend/src/app/shared/pipes/price-transform.pipe.ts
@@ -8,8 +8,11 @@ export class PriceTransformPipe implements PipeTransform {
   transform(value: number): string {
     if (!value) return '';
 
-    // Formatear el número como precio con dos decimales
-    const formattedValue = value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+     // Formatear el número, usando punto para miles y coma para decimales
+     const formattedValue = value
+     .toFixed(2) // Asegura dos decimales
+     .replace('.', ',') // Reemplaza el punto decimal por coma
+     .replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Inserta puntos para los miles
 
     // Agregar el prefijo $ ARS
     return ` $${formattedValue}`;
